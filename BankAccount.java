79cbcd6a71c1ebedffbe4c2a15b47f3f24a930d8
ellakrechmer <ekrechmer20@stuy.edu>
2020-10-26 21:07:31 -0400

    Created a constructor, and added a new class member in the tester

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int id, String pass){
+    balance=0;
+    accountID=id;
+    password=pass;
+  }
+}
--- TestBanker.java
+++ TestBanker.java
@@ -1,5 +1,5 @@
 public static TestBanker.java{
   public static main void(String[] args){
-    
+    account=new BankAccount(221221047, "jajwuth");
   }
 }
